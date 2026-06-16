5660770ba7dc4b7073ea8a47d319b61e86aef2fb
ganesh-webdeveloper <ganeshgundetiwebdeveloper@gmail.com>
2026-06-16 23:17:14 +0530

    folder added new

---
+++ React practice/First.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const first = () => {
+  return (
+      <div>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde architecto et officiis tempora? Praesentium eaque dolorem quibusdam repellendus incidunt esse quo, beatae, eveniet aspernatur atque quisquam eius cum officiis aut.</p>
+    </div>
+  )
+}
+
+export default first
